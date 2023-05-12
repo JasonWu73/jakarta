59eb2a23ca3105844599dd98cad2fd5a64a8dc71
吴仙杰 <wuxianjiezh@gmail.com>
2023-05-12 10:13:04 +0800

    获取项目版本号 API

--- spring-boot-web/src/main/java/net/wuxianjie/springbootweb/SpringBootWebApplication.java
+++ spring-boot-web/src/main/java/net/wuxianjie/springbootweb/SpringBootWebApplication.java
@@ -1,12 +1,50 @@
 package net.wuxianjie.springbootweb;
 
+import net.wuxianjie.springbootweb.auth.AuthProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Spring Boot 启动类。
+ *
+ * <p>将指明项目版本号的 API 置于此处还是比较合适的。
+ *
+ * @author 吴仙杰
+ */
+@RestController
+@RequestMapping("/api/v1")
 @SpringBootApplication
 public class SpringBootWebApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringBootWebApplication.class, args);
+  }
+
+  /**
+   * 获取项目版本号。
+   *
+   * @return 项目版本号信息
+   */
+  @GetMapping("/version")
+  public ResponseEntity<Version> getVersion() {
+    return ResponseEntity.ok(new Version(
+      "v1.0.0",
+      AuthProps.TOKEN_ISSUER,
+      "吴仙杰"
+    ));
+  }
+
+  /**
+   * 项目版本号信息。
+   *
+   * @param version 版本号
+   * @param projectName 项目名
+   * @param developer 开发者
+   */
+  private record Version(String version, String projectName, String developer) {
   }
 }
--- spring-boot-web/src/main/java/net/wuxianjie/springbootweb/auth/WebSecurityConfig.java
+++ spring-boot-web/src/main/java/net/wuxianjie/springbootweb/auth/WebSecurityConfig.java
@@ -106,6 +106,8 @@ public class WebSecurityConfig {
       .authorizeHttpRequests()
       // 开放获取鉴权信息相关 API
       .requestMatchers("/api/v1/token/**").permitAll()
+      // 获取项目版本号 API
+      .requestMatchers("/api/v1/version").permitAll()
       // 默认所有 API 都需要登录才能访问
       .requestMatchers("/**").authenticated().and()
       // 在进入 Spring Security 身份验证过滤器前添加自定义的 Token 身份验证过滤器
