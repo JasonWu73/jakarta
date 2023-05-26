b2fa0128a95f59ddb648364b7cb6f406922627ab
吴仙杰 <wuxianjiezh@gmail.com>
2023-05-26 21:02:45 +0800

    refactor test code

--- spring-boot-web/src/test/java/net/wuxianjie/springbootweb/PasswordEncoderTest.java
+++ spring-boot-web/src/test/java/net/wuxianjie/springbootweb/PasswordEncoderTest.java
@@ -9,7 +9,9 @@ public class PasswordEncoderTest {
   @Test
   void testEncodePassword() {
     final String rawPassword = "111";
+
     final String hashedPassword = new BCryptPasswordEncoder().encode(rawPassword);
+
     Console.log("原密码: {}, 编码后: {}", rawPassword, hashedPassword);
   }
 }
