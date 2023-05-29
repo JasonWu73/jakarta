cd6503360001262221fcc2be8dad455640a69d6d
吴仙杰 <wuxianjiezh@gmail.com>
2023-05-29 16:48:37 +0800

    remove JPA code

--- spring-boot-web/src/main/java/net/wuxianjie/springbootweb/test/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package net.wuxianjie.springbootweb.test;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource(path = "members")
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-}
