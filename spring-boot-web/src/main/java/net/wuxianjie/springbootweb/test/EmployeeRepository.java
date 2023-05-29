25fd9f484fea0a0fdabfd06e71bd4c92ae0984f4
吴仙杰 <wuxianjiezh@gmail.com>
2023-05-29 16:04:35 +0800

    HATEOAS custom

--- spring-boot-web/src/main/java/net/wuxianjie/springbootweb/test/EmployeeRepository.java
+++ spring-boot-web/src/main/java/net/wuxianjie/springbootweb/test/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package net.wuxianjie.springbootweb.test;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
