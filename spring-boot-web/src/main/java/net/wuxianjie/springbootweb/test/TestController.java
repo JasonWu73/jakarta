4ac8bdc82c6c5a7b8352c383a4fe91f845e68705
吴仙杰 <wuxianjiezh@gmail.com>
2023-05-31 15:12:08 +0800

    project setup 1

---
+++ spring-boot-web/src/main/java/net/wuxianjie/springbootweb/test/TestController.java
@@ -0,0 +1,13 @@
+package net.wuxianjie.springbootweb.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+
+  @GetMapping("/")
+  public String showHome() {
+    return "home";
+  }
+}
